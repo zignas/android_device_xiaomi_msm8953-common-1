fa4b0d92288fe1a4ea5c10d569ebaab7b2d19299
Artem Borisov <dedsa2002@gmail.com>
2019-04-04 15:32:38 +0530

    msm8953-common: Hack libmmcamera2_sensor_modules for tissot too
    Change-Id: I4a6bdedb53fb630dd615334f1f14212ea58a3875
    (cherry picked from commit a23bfe2cccfb3bfac37a49510e0588cd40bf5aec)

--- extract-files.sh
+++ extract-files.sh
@@ -75,7 +75,7 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
             "${KANG}" --section "${SECTION}"
 fi
 
-if [ "$DEVICE" = "mido" ]; then
+if [ "$DEVICE" = "mido" ] || [ "$DEVICE" = "tissot" ]; then
     # Hax for cam configs
     CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
     sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
